c219ecd39a864df24772cfed67a17c5f7d5cd991
kategauransh <127082264+kategauransh@users.noreply.github.com>
2026-01-04 03:27:21 +0530

    Daily Java Learning: Creating Threads

--- daily-java-learning/2026-01-04_Multithreading_CreatingThread.java
+++ daily-java-learning/2026-01-04_Multithreading_CreatingThread.java
@@ -1,19 +1,25 @@
-public class Multithreading_CreatingThread {
-
-    // Create a custom thread by extending the Thread class
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            System.out.println("Thread " + Thread.currentThread().getName() + " is running.");
+public class Multithreading_CreatingThread extends Thread {
+    // Override the run method to define the task for the thread
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread " + Thread.currentThread().getName() + " - Count: " + i);
+            try {
+                // Sleep for 500 milliseconds
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
-        // Create and start two threads
-        MyThread thread1 = new MyThread();
-        MyThread thread2 = new MyThread();
-        
-        thread1.start(); // Start thread 1
-        thread2.start(); // Start thread 2
+        // Create two thread instances
+        Multithreading_CreatingThread thread1 = new Multithreading_CreatingThread();
+        Multithreading_CreatingThread thread2 = new Multithreading_CreatingThread();
+
+        // Start both threads
+        thread1.start();
+        thread2.start();
     }
 }
