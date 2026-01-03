68ecfada8034010ee722aa513d02e89306c3c1ab
kategauransh <127082264+kategauransh@users.noreply.github.com>
2026-01-03 16:44:38 +0530

    Daily Java Learning: Multithreading: Creating Threads

--- daily-java-learning/2026-01-03_MultithreadingExample.java
+++ daily-java-learning/2026-01-03_MultithreadingExample.java
@@ -1,15 +1,24 @@
 public class MultithreadingExample extends Thread {
+    // Method that defines the task to be run in a new thread
     public void run() {
-        // Displaying a message from each thread
-        System.out.println("Thread " + Thread.currentThread().getName() + " is running");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread " + Thread.currentThread().getName() + " - Count: " + i);
+            try {
+                // Sleep for a short duration to simulate work
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // Creating and starting two threads
+        // Create two thread instances
         MultithreadingExample thread1 = new MultithreadingExample();
         MultithreadingExample thread2 = new MultithreadingExample();
         
-        thread1.start(); // Starting thread 1
-        thread2.start(); // Starting thread 2
+        // Start both threads
+        thread1.start();
+        thread2.start();
     }
 }
