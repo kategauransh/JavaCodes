84ac009cd179cb5c97de1719148764ac271a4798
kategauransh <127082264+kategauransh@users.noreply.github.com>
2026-01-03 17:49:01 +0530

    Daily Java Learning: Multithreading Basics: Creating a Thread

--- daily-java-learning/2026-01-03_Multithreading_CreatingThread.java
+++ daily-java-learning/2026-01-03_Multithreading_CreatingThread.java
@@ -1,26 +1,13 @@
-public class Multithreading_CreatingThread {
-
-    // This class extends Thread to create a new thread
-    static class MyThread extends Thread {
-        public void run() {
-            // Code that runs in the new thread
-            for (int i = 1; i <= 5; i++) {
-                System.out.println("Thread " + this.getName() + " - Count: " + i);
-                try {
-                    Thread.sleep(500); // Sleep for 500 milliseconds
-                } catch (InterruptedException e) {
-                    System.out.println(e);
-                }
-            }
-        }
+public class Multithreading_CreatingThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread is running!");
     }
 
     public static void main(String[] args) {
-        // Create and start two threads
-        MyThread thread1 = new MyThread();
-        MyThread thread2 = new MyThread();
-        
-        thread1.start(); // Start thread1
-        thread2.start(); // Start thread2
+        // Create a new thread
+        Multithreading_CreatingThread thread = new Multithreading_CreatingThread();
+        // Start the thread
+        thread.start();
     }
 }
